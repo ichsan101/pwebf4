e08af1da1aa62286646f3bf6b032bb5f3e9de183
ichsan101 <muhammadichsan225.mi@gmail.com>
2022-03-21 21:47:51 +0700

    first

--- profile.php
+++ profile.php
@@ -5,11 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/fontawesome.min.css" />
+    <title>Profil</title>
 </head>
 
 <body>
-    <p>Hai</p>
+    <div class="wrapper">
+        <div class="box">
+            <div class="box-img">
+                <img src="images/isan.png" alt="">
+            </div>
+            <div class="box-text">
+                <h1>Muhammad Ichsan Taufiqurrochman</h1>
+                <h3></h3>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
